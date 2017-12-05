10864e4fab1c201f808ddc54e6338f5f45fee667
Nathanaël Martel <nat@nathanaelmartel.net>
2017-12-05 13:45:40 +0100

    config

--- _config.php
+++ _config.php
@@ -3,4 +3,4 @@ $begin_time = microtime(true);
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-$pdo = new PDO('mysql:host=localhost;dbname=imap','root','100282');
+$pdo = new PDO('mysql:host=localhost;dbname=tp4bdd','root','');
