954f49567ccdbfd2eb29455e255dc26f63e03cab
Nathanaël Martel <nat@nathanaelmartel.net>
2017-12-19 17:56:50 +0100

    i18n improved

--- _config.php
+++ _config.php
@@ -8,5 +8,3 @@ $pdo = new PDO('mysql:host=localhost;dbname=imap','root','100282');
 session_start();
 require_once('lib/flash.php');
 require_once('lib/i18n.php');
-
-$lang = 'fr';
--- lib/i18n.php
+++ lib/i18n.php
@@ -1,15 +1,39 @@
 <?php
 
+function get_lang()
+{
+    $langs_available = array('fr', 'en');
+    if (isset($_GET['lang']) && in_array($_GET['lang'], $langs_available)) {
+        return $_GET['lang'];
+    }
+
+    return 'fr';
+}
+
+function load_translation()
+{
+    global $pdo;
+
+    $translations = array();
+
+    $sth = $pdo->prepare('SELECT * FROM setting WHERE lang=?');
+    $sth->execute(array(get_lang()));
+
+    foreach ($sth->fetchAll() as $setting) {
+        $translations[$setting['name']] = $setting['value'];
+    }
+
+    return $translations;
+}
+
+$settings = load_translation();
+
 function __($message)
 {
-    global $lang, $pdo;
+    global $settings;
 
-    $sth = $pdo->prepare('SELECT * FROM setting WHERE name=? AND lang=?');
-    $sth->execute(array($message, $lang));
-    $text = $sth->fetch();
-    
-    if ($text) {
-        return $text['value'];
+    if (isset($settings[$message])) {
+        return $settings[$message];
     }
 
     return $message;
